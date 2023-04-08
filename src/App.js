e60237dd8208fd672e3d45a81189589ecdf2a210
Aditya Kumar Verma <aditya.verma@flipkart.com>
2023-04-08 16:20:01 +0530

    update App.js

--- src/App.js
+++ src/App.js
@@ -1,17 +1,14 @@
 import './App.css';
 import './components/css/Button.css';
 import React, {useState} from 'react';
-import {getPeers} from './api-calls/requests/p2p-peers';
 import {ReturnApp} from "./components/AppUI";
 import config from './config';
-import {getInfo, getP2pInfo} from "./api-calls/requests/p2p-info";
-import {getLocalHead} from "./api-calls/requests/header-local-head";
-import {getSamplingStats} from "./api-calls/requests/daser-sampling-stats";
-import {getBalance} from "./api-calls/requests/state-balance";
-import {getAccountAddress} from "./api-calls/requests/state-account-address";
-import {getResourceState} from "./api-calls/requests/p2p-resource-state";
-import {getProbabilityOfAvailability} from "./api-calls/requests/share-probability-of-availability";
-import {getNodeInfo} from "./api-calls/requests/node-info";
+import {submitPfb} from "./api-calls/requests/submit-pfb";
+import {getNameSpaceSharesAtHeight} from "./api-calls/requests/namespaced-shares";
+import {jsonToHtml} from "./helper/pretty-print-json";
+import {generateRandHexEncodedNamespaceID, textToHex} from "./helper/text-to-hex";
+import {delay} from "./helper/delay";
+
 
 function App() {
     const [loadOnClick, setLoadOnClick] = useState(false);
@@ -19,9 +16,10 @@ function App() {
         console.log('Calculating...');
         addLoadingClass();
         try {
-            await new Promise(resolve => {
-                setTimeout(computeResults, 2000); // Add a delay of 2 seconds
-            });
+            await Promise.all([
+                computeResults(), // Start executing computeResults
+                delay(2000) // Wait for 2 seconds
+            ]);
         } catch (error) {
             console.error(error);
             handleError(config.backend.apiFailureMessage)
@@ -51,71 +49,68 @@ function App() {
         }
     };
 
-    const computeResults = async () => {
-        console.log('loading');
-        let {ipAddress, port, authToken} = readInput();
+    const computeResults = () => {
+        return new Promise(async (resolve) => {
+            console.log("loading");
+            let { namespaceId, textData } = readInput();
+            textData = textToHex(textData);
 
-        // todo: remove this: testing purpose
-        // ipAddress = "165.232.182.75"
-        // port = "26658"
-        // authToken = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJBbGxvdyI6WyJwdWJsaWMiLCJyZWFkIiwid3JpdGUiLCJhZG1pbiJdfQ.6Vt55wvPNw1uk5z0NB4ufPj-IOnR77pG7i0LjwtRkOU"
+            console.log(namespaceId, textData);
 
-
-        console.log(ipAddress, port, authToken)
-
-        if (!ipAddress || !port || !authToken) {
-            handleError(config.missingValuesMessage);
-            return;
-        }
-
-        try {
-            const [
-                peerCount,
-                info,
-                localHead,
-                samplingStats,
-                // balance,
-                accountAddress,
-                // resourceState,
-                probabilityOfAvailability,
-                apiVersion
-            ] = await Promise.all([
-                getPeers(ipAddress, port, authToken),
-                getP2pInfo(ipAddress, port, authToken),
-                getLocalHead(ipAddress, port, authToken),
-                getSamplingStats(ipAddress, port, authToken),
-
-                // getBalance(ipAddress, port, authToken),
-                getAccountAddress(ipAddress, port, authToken),
-                // getResourceState(ipAddress, port, authToken),
-                getProbabilityOfAvailability(ipAddress, port, authToken),
-                getNodeInfo(ipAddress, port, authToken)
-            ]);
-
-
-            console.log(`peerCount: ${peerCount}`)
-
-            if ([peerCount, info, localHead, accountAddress, probabilityOfAvailability, samplingStats, apiVersion].some((value) => value === null || value === undefined || Number.isNaN(value))) {
-                handleError(config.backend.apiFailureMessage);
-                return;
+            if (!namespaceId || !textData) {
+                handleError(config.missingValuesMessage);
+                return resolve(); // Resolve the promise if there's an error
             }
 
-            updateUI(peerCount, info, localHead, accountAddress, probabilityOfAvailability, samplingStats, apiVersion);
-            removeLoadingClass();
-            setLoadOnClick(true);
-        } catch (e) {
-            console.log(config.backend.apiFailureMessage)
-            handleError(config.backend.apiFailureMessage);
-        }
+            try {
+                const [height] = await Promise.all([submitPfb(namespaceId, textData)]);
 
+                console.log(`height: ${height}`);
+
+                if (
+                    [height].some(
+                        (value) =>
+                            value === null || value === undefined || Number.isNaN(value)
+                    )
+                ) {
+                    handleError(config.backend.apiFailureMessage);
+                    return resolve(); // Resolve the promise if there's an error
+                }
+
+                const sharesAtHeight = await getNameSpaceSharesAtHeight(
+                    namespaceId,
+                    height
+                );
+
+                if (
+                    [sharesAtHeight].some(
+                        (value) =>
+                            value === null || value === undefined || Number.isNaN(value)
+                    )
+                ) {
+                    handleError(config.backend.apiFailureMessage);
+                    return resolve(); // Resolve the promise if there's an error
+                }
+
+                const uiResponsePrettyJson = await jsonToHtml(sharesAtHeight);
+                updateUI(namespaceId, uiResponsePrettyJson);
+                removeLoadingClass();
+                setLoadOnClick(true);
+            } catch (e) {
+                console.log(config.backend.apiFailureMessage);
+                handleError(config.backend.apiFailureMessage);
+            }
+
+            resolve(); // Resolve the promise at the end of the function
+        });
     };
 
-    const readInput = () => {
-        const ipAddress = document.getElementById('field-0').value;
-        const port = document.getElementById('field-1').value;
-        const authToken = document.getElementById('field-2').value;
 
-        return {ipAddress, port, authToken};
+    const readInput = () => {
+        const namespaceId = document.getElementById('field-0').value || generateRandHexEncodedNamespaceID();
+        const textData = document.getElementById('textarea-0').value;
+
+        return {namespaceId, textData};
     };
 
     const handleError = (message) => {
@@ -123,17 +118,9 @@ function App() {
         removeLoadingClass();
     };
 
-    const updateUI = (peerCount, info, localHead, accountAddress, probabilityOfAvailability, samplingStats, apiVersion) => {
-        document.getElementById('resultBox-0').innerHTML = info
-        document.getElementById('resultBox-1').innerHTML = accountAddress
-        document.getElementById('resultBox-2').innerHTML = localHead
-        document.getElementById('resultBox-3').innerHTML = peerCount
-        document.getElementById('resultBox-4').innerHTML = probabilityOfAvailability
-        document.getElementById('resultBox-5').innerHTML = samplingStats.isRunning
-        document.getElementById('resultBox-6').innerHTML = samplingStats.catchUpStatus
-        document.getElementById('resultBox-7').innerHTML = samplingStats.headOfCatchup
-        document.getElementById('resultBox-8').innerHTML = samplingStats.headOfSampledChain
-        document.getElementById('resultBox-9').innerHTML = apiVersion
+    const updateUI = (namespaceId, uiResponsePrettyJson) => {
+        document.getElementById('resultBox-0').innerHTML = namespaceId
+        document.getElementById('resultBox-1').innerHTML = uiResponsePrettyJson
     };
 
     return (<ReturnApp load={load} config={config}/>);
